5eaa9c998a088049e9f3ce6e838d0e54b3f8d7d0
Denis Nikolaev <Disa1331@yandex.ru>
2025-12-12 17:00:01 +0400

    Add files via upload

---
+++ utils_vkusvill/config.py
@@ -0,0 +1,13 @@
+# Пути к необходимым файлам (датасеты, база данных SQLite, json-ключ)
+input_file_events = '/opt/airflow/dags/vkusvill/events.csv'
+input_file_orders = '/opt/airflow/dags/vkusvill/orders.csv'
+db_path = '/opt/airflow/dags/vkusvill/analysis_vv.db'
+output_file = '/opt/airflow/dags/vkusvill/processed_data.csv'
+output_file_funnel = '/opt/airflow/dags/vkusvill/funnel.csv'
+output_file_metrics = '/opt/airflow/dags/vkusvill/metrics.csv'
+json_keyfile = '/opt/airflow/dags/vkusvill/project-vkusvill-e6e3f5b46011.json'
+csv_files = {
+    output_file: '1dhfnzmz77qUaNIaHfqyNYAUYfAxYakT-Uw_6kNqAJgQ',
+    output_file_funnel: '1mhJrMUPOZ0mUwoO3ZrrZj6ofaaq7GJFQEHHaY4u1rF0',
+    output_file_metrics: '1F7Tl6Lj1MkouH4-G3FW1HtHVQKFN3gOBUQIkBvSxlZY'
+}
